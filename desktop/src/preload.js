7cdfab67c77496c87d3cb0ed7dc9eb8305047b0d
richlifetechnologies-lang <richlifetechnologies@gmail.com>
2026-05-17 08:48:48 -0600

    feat: expose window controls in preload (v1.0.15)

--- desktop/src/preload.js
+++ desktop/src/preload.js
@@ -1,12 +1,20 @@
-const { contextBridge, ipcRenderer } = require("electron");
+const { contextBridge, ipcRenderer } = require('electron');
 
-// Expose safe auto-updater API to the renderer (web app running in the window)
-contextBridge.exposeInMainWorld("electronUpdater", {
-  onUpdateAvailable: (cb) => ipcRenderer.on("update-available", (_e, version) => cb(version)),
-  onUpdateDownloaded: (cb) => ipcRenderer.on("update-downloaded", cb),
-  downloadUpdate: () => ipcRenderer.send("download-update"),
-  installUpdate: () => ipcRenderer.send("install-update"),
+// Expose safe auto-updater API to the renderer
+contextBridge.exposeInMainWorld('electronUpdater', {
+  onUpdateAvailable: (cb) => ipcRenderer.on('update-available', (_e, version) => cb(version)),
+  onUpdateDownloaded: (cb) => ipcRenderer.on('update-downloaded', cb),
+  downloadUpdate: () => ipcRenderer.send('download-update'),
+  installUpdate: () => ipcRenderer.send('install-update'),
 });
 
 // Expose Electron flag so the web app can detect it's running in the desktop
-contextBridge.exposeInMainWorld("isElectron", true);
+contextBridge.exposeInMainWorld('isElectron', true);
+
+// Expose window controls for custom title bar
+contextBridge.exposeInMainWorld('electronWindow', {
+  minimize: () => ipcRenderer.send('window-minimize'),
+  maximize: () => ipcRenderer.send('window-maximize'),
+  close: () => ipcRenderer.send('window-close'),
+  isMaximized: (cb) => ipcRenderer.on('window-maximized-state', (_e, state) => cb(state)),
+});
